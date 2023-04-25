4bae41e7636d83859b095990d4fd37f16b8937fd
Jeppe Hauman <jeppe_hauman@hotmail.com>
2023-04-25 14:23:04 +0200

    supabase, images stored in cloudinary with url stored in db

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <header className="z-50 h-16 fixed bg-stone-500 w-full top-0">
       <nav className="flex gap-5 justify-center items-center h-full">
-        <div className="h-full px-2 flex items-center text-white hover:bg-slate-100 hover:cursor-pointer hover:text-stone-500">
+        <div className="h-full px-2 flex items-center text-stone-100 hover:bg-slate-100 hover:cursor-pointer hover:text-stone-500">
           <Link className="text-xl font-semibold" href={"/"}>
             Home
           </Link>
         </div>
-        <div className="h-full px-2 flex items-center text-white hover:bg-slate-100 hover:cursor-pointer hover:text-stone-500">
+        <div className="h-full px-2 flex items-center text-stone-100 hover:bg-slate-100 hover:cursor-pointer hover:text-stone-500">
           <Link className="text-xl font-semibold" href={"/leashes"}>
             Leashes
           </Link>
--- app/leashes/page.tsx
+++ app/leashes/page.tsx
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -16,8 +17,8 @@ function Card({
 }: CardProps) {
   return (
     <div className="h-[500px] max-w-xs w-full flex flex-col rounded-md hover:shadow-2xl">
-      <div className="relative h-3/4">
-        <Link href={`product/1`}>
+      <div className="h-3/4">
+        <Link className="h-full relative w-full block" href={`/`}>
           <Image
             className="rounded-md"
             fill={true}
@@ -27,11 +28,8 @@ function Card({
         </Link>
       </div>
 
-      <div className="px-12 py-4 text-white font-semibold">
-        <Link
-          href={`product/1`}
-          className=" flex flex-col items-center justify-center"
-        >
+      <div className="px-12 py-4 text-stone-100 font-semibold">
+        <Link href={`/`} className=" flex flex-col items-center justify-center">
           <p className="w-fit">{productName}</p>
 
           <p
@@ -53,60 +51,33 @@ function Card({
   );
 }
 
-export default function LeashesPage() {
+export const revalidate = 10;
+
+const getData = async () => {
+  const leashes = await prisma.product.findMany();
+  return leashes;
+};
+
+export default async function LeashesPage() {
+  const leashes = await getData();
+
   return (
     <div className="mt-16 px-6">
+      <h1 className="text-6xl text-center my-8 text-stone-100">Leashes</h1>
       <div className="grid md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4 max-w-5xl mx-auto">
         <Card
           imgSrc="/hamza-gharnati-small.jpg"
           productName="Leather leash - Black"
           productPrice={100}
         />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
-        <Card
-          imgSrc="/hamza-gharnati-small.jpg"
-          productName="Leather leash - Black"
-          productPrice={100}
-        />
+        {leashes?.map((leash: any) => (
+          <Card
+            key={leash.id}
+            productName={leash.title}
+            productPrice={leash.price}
+            imgSrc={leash.imgSrc}
+          />
+        ))}
       </div>
     </div>
   );
